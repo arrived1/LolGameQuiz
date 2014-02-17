4430107f2f36dac9d13309d49e5b11c18963a063
Radoslaw Przybyl <Radoslaw.Przybyl@sabre.com>
2014-02-17 22:03:05 +0000

    fixed gradle, added google play service

--- LolGameQuiz/src/main/java/com/lolgamequiz/my/GameFragment.java
+++ LolGameQuiz/src/main/java/com/lolgamequiz/my/GameFragment.java
@@ -12,8 +12,11 @@ import android.widget.ImageView;
 import com.lolgamequiz.my.GameEngine.Animations;
 import com.lolgamequiz.my.GameEngine.GameSounds;
 
+
 public class GameFragment extends Fragment {
+
     private Animations animations;
+
 
     public GameFragment(){}
 
@@ -21,17 +24,15 @@ public class GameFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_game, container, false);
-
         Context context = getActivity();
 
         GameSounds sounds = new GameSounds((Activity)context);
         sounds.welcome();
 
-        animations = new Animations(context);
-
         RateUs.app_launched(context);
         //RateUs.showRateDialog(context, null); //test purposes
 
+        animations = new Animations(context);
         ImageView image = (ImageView)rootView.findViewById((R.id.logo));
         image.setAnimation(animations.getAnimFadein());
 
